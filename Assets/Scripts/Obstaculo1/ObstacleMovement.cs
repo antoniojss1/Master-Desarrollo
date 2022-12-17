c83a83e62af75b59bda943224259c2f001f9178f
EnmanuelFR <120492287+EnmanuelFR@users.noreply.github.com>
2022-12-17 13:11:20 +0100

    Prefab y codigo obstaculo 1

---
+++ Assets/Scripts/Obstaculo1/ObstacleMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private float speed = 2;
+    private int waypointIndex = 0;
+
+    //To stay in position
+    private bool moveNext = true;
+    [SerializeField] float timeToNext = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlatform();
+    }
+
+    void movePlatform()
+    {
+        if (moveNext)
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, waypoints[waypointIndex].transform.position) < 0.1f)
+        {
+            StartCoroutine(TimeMove());
+            waypointIndex++;
+            if (waypointIndex >= waypoints.Length)
+                waypointIndex = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.gameObject.transform.SetParent(null);
+    }
+
+    //Waiting coroutine
+    IEnumerator TimeMove()
+    {
+        moveNext = false;                               //Before time passing
+        yield return new WaitForSeconds(timeToNext);
+        moveNext = true;                                //After time passing
+    }
+}
