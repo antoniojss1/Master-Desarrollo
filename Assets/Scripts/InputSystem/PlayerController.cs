58a686f86a2f7de64a866727a5a3a25d250bd7af
EnmanuelFR <120492287+EnmanuelFR@users.noreply.github.com>
2022-12-30 16:53:59 +0100

    Cambios en salto jugador

--- Assets/Scripts/InputSystem/PlayerController.cs
+++ Assets/Scripts/InputSystem/PlayerController.cs
@@ -7,62 +7,79 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInput _playerInput;
     private InputAction _accionAndar;
+    private InputAction _accionMirar;
     private InputAction _accionSaltar;
+    private CharacterController _charController;
+    private Vector3 movementVector;
 
-    [SerializeField] float walkSpeed = 5f;
-    [SerializeField] float jumpPower = 1f;
+    [SerializeField] private float speed = 15f;
+    [SerializeField] private float sightSensitivity = 40f;
+    [SerializeField] private float jumpPower = 3f;
+    [SerializeField] private float gravityMult = 1f;
 
-    private bool _groundedPlayer;
+    private float _gravity = -9.81f;
+    private float _velocity;
 
-    Vector2 moveInput;
-    Rigidbody myRigidbody;
-
+    private float xRotation;
+    private Camera _mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody>();
-
-        _playerInput = GetComponent<PlayerInput>();
+        _playerInput = GetComponent<PlayerInput>(); //Porque es un componente de la misma entidad (GameObject)
         _accionAndar = _playerInput.actions["Andar"];
+        _accionMirar = _playerInput.actions["Mirar"];
         _accionSaltar = _playerInput.actions["Saltar"];
+        this._charController = GetComponent<CharacterController>();
+        this._mainCamera = GetComponentInChildren<Camera>(); //Buscar componente cámara dentro de objetos hijos (solo es el objeto Camera).
     }
 
     // Update is called once per frame
     void Update()
     {
-        Run();
+        ApplyGravity();
+        walkAction();
+        lookAction();
+        JumpAction();
+    }
 
+    //Cada funcionalidad con un método aparte
+    private void walkAction()
+    {
+        movementVector = this.transform.right * _accionAndar.ReadValue<Vector2>().x + this.transform.forward * _accionAndar.ReadValue<Vector2>().y + this.transform.up * movementVector.y;
+        this._charController.Move(movementVector * Time.deltaTime * this.speed);
+    }
 
-        if (_accionSaltar.ReadValue<float>() > 0)
+    private void lookAction()
+    {
+        float mouseX = _accionMirar.ReadValue<Vector2>().x * sightSensitivity * Time.deltaTime;
+        float mouseY = _accionMirar.ReadValue<Vector2>().y * sightSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -45, 45);
+
+        _mainCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);  
+
+        this.transform.Rotate(Vector3.up * mouseX);                                 
+    }
+
+    private void JumpAction()
+    {
+        if (_accionSaltar.triggered && _charController.isGrounded)
         {
-            if (isGrounded()) 
-            {
-                Jump();
-            }
+            _velocity += jumpPower;
         }
-        
     }
 
-    void Run()
+    private void ApplyGravity()
     {
-        Vector3 playerVelocity = new Vector3(_accionAndar.ReadValue<Vector2>().x * walkSpeed, myRigidbody.velocity.y, _accionAndar.ReadValue<Vector2>().y * walkSpeed);
-        myRigidbody.velocity = transform.TransformDirection(playerVelocity);
+        if (_charController.isGrounded && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMult * Time.deltaTime;
+        }
+        movementVector.y = _velocity;
     }
-
-    void Jump()
-    {
-        myRigidbody.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
-    }
-
-    void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
-
-    bool isGrounded() 
-    {
-        return myRigidbody.velocity.y == 0;
-        //Physics.CheckSphere(groundCheck.position, .1f)
-    }
-
 }
